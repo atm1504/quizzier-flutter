0ef4ea0d2b7bc5257c84b214cba84f85381c94a3
Amartya Mondal <atm1504.in@gmail.com>
2023-07-09 10:45:29 +0530

    Qa added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(Quizzler());
@@ -25,20 +27,14 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Widget> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
+  List<Widget> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
+  List<bool> answers = [false, true, true];
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -51,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -76,13 +72,22 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
+                bool correctAnswer = answers[questionNumber];
+                Icon widg;
+                if (correctAnswer == true) {
+                  widg = Icon(
+                    Icons.check,
+                    color: Colors.green,
                   );
+                } else {
+                  widg = Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  );
+                }
+                setState(() {
+                  questionNumber++;
+                  scoreKeeper.add(widg);
                 });
               },
             ),
@@ -103,6 +108,23 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = answers[questionNumber];
+                Icon widg;
+                if (correctAnswer == false) {
+                  widg = Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  );
+                } else {
+                  widg = Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  );
+                }
+                setState(() {
+                  questionNumber++;
+                  scoreKeeper.add(widg);
+                });
                 //The user picked false.
               },
             ),
